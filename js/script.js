6f54a25c9c263d6f80fb551319b1140b6fc0d2f9
Luciano Souza <118829644+LucianoRSouza@users.noreply.github.com>
2026-03-23 12:03:46 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1660,6 +1660,14 @@ function initLoading() {
     window.addEventListener('load', hideLoading);
   }
 }
+  
+  // Se a página já estiver completamente carregada, esconde imediatamente
+  if (document.readyState === 'complete') {
+    hideLoading();
+  } else {
+    window.addEventListener('load', hideLoading);
+  }
+}
 
 function initSmoothAnchors() {
   $$('a[href^="#"]').forEach(a => {
