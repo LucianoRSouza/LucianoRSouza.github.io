d691d3117bb459bc3862268ca127af3507e69e60
Luciano Souza <118829644+LucianoRSouza@users.noreply.github.com>
2026-03-24 12:16:10 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,7 +36,9 @@ function initLoading() {
 
 function hideLoading(loading) {
   if (!loading || loading.classList.contains('hidden')) return;
+
   loading.classList.add('hidden');
+
   setTimeout(() => {
     if (loading.parentNode) loading.parentNode.removeChild(loading);
   }, 500);
